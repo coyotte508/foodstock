ab558bd72ab6a172039e6e74b0bdae06318016e8
coyotte508 <coyotte508@gmail.com>
2019-01-30 04:37:30 +0100

    Skeleton for levelup and place helper actions

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,4 +1,4 @@
-import {Game} from 'boardgame.io/core';
+import {Game, INVALID_MOVE} from 'boardgame.io/core';
 import { Player, createPlayer } from './player';
 import * as _ from 'lodash';
 import { numberOfCustomers, numberOfSpecialCustomers } from './customer';
@@ -12,16 +12,25 @@ interface Context {
   random: {
     Shuffle: <T>(deck: T[]) => T[],
   };
+  events: {
+    endTurn: () => void,
+    endPhase: () => void,
+  };
+  currentPlayer: string;
+  /** Player that just moved */
+  playerID: string;
+  turn: number;
 }
 
 interface GameState {
   secret: null;
 
   players: {
-    [key: number]: Player,
+    [key: string]: Player,
   };
 
   nPlayers: number;
+  round: number;
 
   customers: number[];
   specialCustomers: number[];
@@ -38,6 +47,7 @@ const Foodstock = Game({
 
       customers: _.range(0, numberOfCustomers),
       specialCustomers: _.range(0, numberOfSpecialCustomers),
+      round: 1,
     };
 
     for (let i = 0; i < ctx.numPlayers; i++) {
@@ -50,8 +60,31 @@ const Foodstock = Game({
     return G;
    },
   moves: {
-    // Todo: action that changes the state of the game, and returns modified state
-    action1: (G: GameState) => G,
+    // When moving to the next board
+    levelUp: (G: GameState, ctx: Context, payload: any) => {
+      const pl = G.players[ctx.currentPlayer];
+
+      if (pl.level >= 6) {
+        return INVALID_MOVE;
+      }
+
+      pl.level += 1;
+      ctx.events.endTurn();
+      return G;
+    },
+
+    // When placing a helper
+    placeHelper: (G: GameState, ctx: Context, payload: any) => {
+      ctx.events.endTurn();
+      return G;
+    },
+  },
+
+  flow: {
+    // End condition of the game
+    endGameIf: (G: GameState, ctx: Context) => {
+      return (G.nPlayers < 4 && G.round >= 4) || G.round >= 5;
+    },
   },
 
   // State viewable by specific player (hide cards)
--- src/engine/player.ts
+++ src/engine/player.ts
@@ -1,7 +1,11 @@
 export interface Player {
   money: number;
+  level: number;
 }
 
 export function createPlayer() {
-  return {money: 0};
+  return {
+    money: 0,
+    level: 1,
+  };
 }
