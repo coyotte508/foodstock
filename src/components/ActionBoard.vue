e7fbe3f71a9571702c25f9fb591ff0a48ebf0046
coyotte508 <coyotte508@gmail.com>
2019-02-02 02:16:40 +0100

    Display player indicator on board

--- src/components/ActionBoard.vue
+++ src/components/ActionBoard.vue
@@ -8,6 +8,10 @@
         <Helper v-if="action(i,j).helpers.length > 0" :id="action(i,j).helpers[0]" />
       </g>
     </g>
+
+    <g v-for="(id,i) in players" :key="'player-' + i" :transform="`translate(2, ${i*3 + 1.7})`">
+      <Helper :id=id />
+    </g>
   </svg>
 </template>
 
@@ -16,6 +20,7 @@ import {Vue, Component, Prop} from "vue-property-decorator";
 import boardZones from '../graphics/board-zones';
 import Helper from './Helper.vue';
 import { Level } from '@/engine/enums';
+import { Player } from '@/engine/player';
 
 @Component({
   components: {
@@ -32,6 +37,14 @@ export default class ActionBoard extends Vue {
 
   highlighted(i, j): boolean {
     return this.$store.state.foodstock.extra.highlight.boardZones.some(zone => zone[0] === this.number && zone[1] === i && zone[2] === j);
+  }
+
+  get players() {
+    return ['0', '1', '2', '3'].filter(id => this.player(id) && this.player(id).level === this.number);
+  }
+
+  player(id): Player {
+    return this.$store.state.foodstock.game.players[id];
   }
 
   action(i, j) {
--- src/components/PlayerBoard.vue
+++ src/components/PlayerBoard.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="player-info ma-2 pt-2 text-xs-center" :style="`border: solid 1px ${color}`">
+  <v-card class="player-info ma-2 pt-2 text-xs-center" :style="`border: solid 1px ${color}`" :flat="!active" :raised="active" hover>
     <h3>Player {{+id + 1}}</h3>
     <svg viewBox="0 0 113 80" class="player-board">
       <use href="#player-board" :x=0 :y=0 />
       <Helper :transform="`translate(${3.4+4.56*h},77)`" v-for="h in helpers" :key="'helper-' + h" :id=id />
     </svg>
-  </div>
+  </v-card>
 </template>
 
 <script lang="ts">
@@ -34,6 +34,10 @@ export default class PlayerBoard extends Vue {
 
   get color() {
     return playerColor(this.id);
+  }
+
+  get active() {
+    return this.$store.state.foodstock.context.currentPlayer === this.id;
   }
 }
 
