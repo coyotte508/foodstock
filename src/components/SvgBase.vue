f5bf47c7ea81b58b9b93dc050244ccca9b483c04
coyotte508 <coyotte508@gmail.com>
2019-02-14 13:41:31 +0100

    Move assets

--- src/components/SvgBase.vue
+++ src/components/SvgBase.vue
@@ -5,6 +5,14 @@
         <image xlink:href="../assets/board1.png" height="100" width="60" id="board1" />
         <image xlink:href="../assets/board2.png" height="100" width="60" id="board2" />
         <image xlink:href="../assets/player-board.png" width="113" height="80" id="player-board" />
+
+        <image xlink:href="../assets/customers/basic/basicCard-0.jpg" width="76" height="104" id="customer-basic-0" />
+        <image xlink:href="../assets/customers/basic/basicCard-1.jpg" width="76" height="104" id="customer-basic-1" />
+        <image xlink:href="../assets/customers/basic/basicCard-2.jpg" width="76" height="104" id="customer-basic-2" />
+
+        <image xlink:href="../assets/customers/special/card-0.jpg" width="76" height="104" id="customer-special-0" />
+        <image xlink:href="../assets/customers/special/card-1.jpg" width="76" height="104" id="customer-special-1" />
+        <image xlink:href="../assets/customers/special/card-2.jpg" width="76" height="104" id="customer-special-2" />
       </defs>
     </defs>
   </svg>
