3950a0b7de34c481ea65462f7ede2088233c5cd6
riccardo.zulian <riccardo.zulian@gmail.com>
2019-02-21 11:38:14 +0100

    fix createBasicCustomerDeck

--- src/engine/customer.ts
+++ src/engine/customer.ts
@@ -30,7 +30,7 @@ export function createBasicCustomerDeck() {
   const deckComposition = [7, 5, 7];
   const deck = [];
   for (let i = 0; i < deckComposition.length; i++) {
-    deck.push(... new Array(deckComposition[i]).map(() => i));
+    deck.push(... [... new Array(deckComposition[i])].map(() => i));
   }
   // deckComposition.forEach( c => deck.splice( deck.length , 0, ...Array.from(Array(deckComposition[c]), x => c)));
   return deck;
