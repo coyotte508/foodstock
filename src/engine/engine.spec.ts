39586d243bdba7ad53c117b3a5408eaad8248b1c
riccardo.zulian <riccardo.zulian@gmail.com>
2019-02-21 13:34:36 +0100

    test for levelUp with new customer

--- src/engine/engine.spec.ts
+++ src/engine/engine.spec.ts
@@ -21,11 +21,28 @@ describe("engine", () => {
   it("should add a basic Customer from TopDeck when level up a player", () => {
     const client = new FoodstockClient();
 
+
     expect(client.player(0).level).to.equal(Level.Level1);
     client.moves.levelUp();
+    const numCards =  client.G.customers.basic.deck.length;
     const card = client.G.customers.basic.deck[0];
     client.moves.gainCustomer(  {which: CardPosition.TopDeck, special: false});
+    expect(client.player(0).customers.waiting[0].id).to.equal("0");
+    expect(client.G.customers.basic.deck.length).to.equal(numCards - 1);
 
+  });
+
+  it("should add a basic Customer from available cards when level up a player", () => {
+    const client = new FoodstockClient();
+
+
+    expect(client.player(0).level).to.equal(Level.Level1);
+    client.moves.levelUp();
+    const numCards =  client.G.customers.basic.deck.length;
+    const card = client.G.customers.basic.deck[0];
+    client.moves.gainCustomer(  {which: CardPosition.First, special: false});
+    expect(client.player(0).customers.waiting[0].id).to.equal("1");
+    expect(client.G.customers.basic.deck.length).to.equal(numCards - 1);
 
   });
   // it("should prevent placing a helper in the same spot twice", () => {
